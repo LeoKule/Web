432a2408c26bf65307cd6e5b6e5996666b8dc185
Leonid <kylev77@list.ru>
2024-03-03 16:49:01 +0300

    Added task 14

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -48,3 +48,127 @@ echo "Task 12<br/>";
 echo 8**2; // the first number is raised to the power of the second
 echo "<br/>";
 echo"\n";
+
+//Task 13
+echo "Task 13<br/>";
+$myNum = 19;
+$answer = $myNum;
+$answer += 2;
+$answer *= 2;
+$answer -= 2;
+$answer /= 2;
+$answer -= $myNum;
+echo $answer;
+echo "<br/>";
+echo "\n";
+
+//Task 14
+echo "Task 14<br/>";
+$a = 10;
+$b = 3;
+$k = 16;
+$m = 4;
+
+echo $a % $b;
+echo "<br/>";
+echo "\n";
+
+if ($a % $b === 0){
+    echo "Делится ", $a / $b, "\n";
+    echo "<br/>";
+}
+else echo "Делится с остатком ", $a % $b, "\n";
+echo "<br/>";
+
+if ($k % $m === 0){
+    echo "Делится ", $k / $m, "\n";
+    echo "<br/>";
+}
+else echo "Делится с остатком ", $k % $m, "\n";
+
+
+$st = pow(2,10);
+echo $st, "\n";
+echo "<br/>";
+
+$sqrt245 = sqrt(245);
+echo $sqrt245, "\n" , "<br/>";
+
+$arr = [4, 2, 5, 19, 13, 0, 10];
+$sum = 0;
+
+foreach ($arr as &$value) {
+    $sum += pow($value,2); //Cумма всех элементов в квадрате
+}
+
+$sum = sqrt($sum);
+echo $sum,"\n", "<br/>";
+
+$sqrt = sqrt(379);
+echo round($sqrt), " ", round($sqrt, 1), " ", round($sqrt,2), "\n", "<br/>";  //round - округляет до укзанного приближения числа, 19, 19.1, 19.13 и т д
+
+$sqrt = sqrt(587);
+$arr = ['floor' => floor($sqrt), 'ceil' => ceil($sqrt)];
+
+echo $arr['floor'], " ", $arr['ceil'], "\n", "<br/>";  //ceil - округление в большую, floor - в меньшую
+//floor и ceil - ключи. Индексов здесь нет, имеем только ключи, следовательно обратиться к $arr[0] не получится.
+
+$arr = [4, -2, 5, 19, -130, 0, 10];
+$min = min($arr);
+$max = max($arr);
+echo $min," ", $max, "\n", "<br/>";
+
+$z=rand(1,100);
+echo $z,"\n", "<br/>";
+
+for ($i = 0; $i < 10; $i++){
+    $arr[$i] = rand(1,100);
+}
+for ($i = 0; $i < 10; $i++){
+    echo $arr[$i], " ";
+}
+echo "\n", "<br/>";
+
+$a = 5;
+$b = 6;
+echo abs($a - $b), "\n", "<br/>";  //проверил, работает с разными а и б
+
+$arr = [1, 2, -1, -2, 3, -3];
+
+for ($i = 0; $i < count($arr); $i++){   //Взял модули от каждого числа в массиве
+    $arr[$i] = abs($arr[$i]);
+}
+for ($i = 0; $i < count($arr); $i++){
+    echo $arr[$i], " ";
+}
+echo "\n", "<br/>";
+
+$h = 69;// Алгоритм заключается в том, что бы берём 1 и идём до самого числа, далее проверяем, делится ли число h на делитель. Да - пушим в массив. Нет так нет(.
+$divider = 1;
+$i = 0;
+$arr = [1];
+while ($divider <= $h){
+    if($h % $divider === 0){
+        $arr[$i] = $divider;
+        $i++;
+    }
+    $divider++;
+}
+
+for ($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i], " ";
+}
+echo "\n", "<br/>";
+
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$max_sum = 10;
+$arr_sum = 0;
+$count = 0;
+for ($i = 0; $i < count($arr); $i++){ // проходим
+    $arr_sum += $arr[$i]; // суммируем
+    $count++; // кол-во
+    if ($arr_sum > $max_sum){ // проверка
+        break;
+    }
+}
+echo $count, "\n", "<br/>";
