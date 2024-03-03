770cfc53325cd40c6ffc962f754010de68287f6f
Leonid <kylev77@list.ru>
2024-03-03 14:11:09 +0300

    Add task 1

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -1,3 +1,11 @@
 <?php
-echo phpinfo();
-//echo "Hello world!";
+//Task 1
+echo "Task 1<br/>";
+echo "\n";
+$very_bad_unclear_name = "15 chicken wings";
+//my code here:
+$order = &$very_bad_unclear_name; // т.к $order инициализирована  с помощью ссылки на $very_bad_unclear_name, то у них один и тот же адресс в памяти.
+                                  // Значит если мы изменим одну переменную, то поменяется и вторая.
+$order = " and 2 delicious leg<br/>";
+echo "\nYour order is: $very_bad_unclear_name";
+echo "\n";
