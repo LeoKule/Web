fdbfab724583396db26d713b1f48925c55b0fa91
Leonid <kylev77@list.ru>
2024-06-04 22:35:17 +0300

    added

--- Lab5/code/index.php
+++ Lab5/code/index.php
@@ -15,5 +15,45 @@
 </form>
 
 <h1>Все объявления</h1>
+
+<?php
+// подключение к базе данных
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if ($mysqli->connect_errno) {
+    printf("Подключение к серверу MySQL невозможно, код ошибки: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+// получение всех данных из таблицы
+$result = $mysqli->query("SELECT * FROM web.ad");
+$data = [];
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+// закрытие соединения с базой данных
+$mysqli->close();
+?>
+
+<table>
+    <tr>
+        <th>Email</th>
+        <th>Название</th>
+        <th>Описание</th>
+        <th>Категория</th>
+    </tr>
+    <?php foreach ($data as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['email']); ?></td>
+            <td><?= htmlspecialchars($row['title']); ?></td>
+            <td><?= htmlspecialchars($row['description']); ?></td>
+            <td><?= htmlspecialchars($row['category']); ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
 </body>
 </html>
